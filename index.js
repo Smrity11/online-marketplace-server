66843cf66016a83081177a9833ad6169fee646be
Smrity11 <kanisasia376@gmail.com>
2023-11-06 22:27:18 -0800

    add jwt all method with cookie parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,10 +15,11 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikoswdf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,26 +31,105 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// middlewares 
+const logger = (req, res, next) =>{
+  console.log('log: info', req.method, req.url);
+  next();
+}
+
+const verifyToken = (req, res, next) =>{
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  // no token available 
+  if(!token){
+      return res.status(401).send({message: 'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+      if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+      }
+      req.user = decoded;
+      next();
+  })
+}
+
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const postjobCollection = client.db("courseDB").collection('allpostJob')
 
-    app.get('/postjob' ,async(req ,res ) =>{
-      const cursor = postjobCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-  })
+         // auth related api
+         app.post('/jwt', logger, async (req, res) => {
+          const user = req.body;
+          console.log('user for token', user);
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
-    app.post('/postjob' , async(req,res) =>{
+          res.cookie('token', token, {
+              httpOnly: true,
+              secure: true,
+              sameSite: 'none'
+          })
+              .send({ success: true });
+      })
+
+      app.post('/logout', async (req, res) => {
+          const user = req.body;
+          console.log('logging out', user);
+          res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+      })
+
+
+
+      app.get('/allpostJob', logger, verifyToken, async (req, res) => {
+          console.log(req.query.email);
+            console.log('token info', req.user)
+            if(req.user.email !== req.query.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+
+        let query = {};
+        if (req.query?.email) {
+            query = { email: req.query.email }
+        }
+        const result = await postjobCollection.find(query).toArray();
+        res.send(result);
+    })
+
+       app.post('/allpostJob' , async(req,res) =>{
       const newjob = req.body
       console.log(newjob);
       const result = await postjobCollection.insertOne(newjob)
       res.send(result)
   })
 
+  //   app.get('/postjob' ,async(req ,res ) =>{
+  //     const cursor = postjobCollection.find()
+  //     const result = await cursor.toArray()
+  //     res.send(result)
+  // })
+
+
+    app.get('/allpostJob/id' ,async(req ,res ) =>{
+      const cursor = postjobCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+  })
+
+
+  // post method
+ 
+
+
+
+
+// delete method
     app.delete("/postjob/:id" ,async(req , res ) =>{
         const id = req.params.id 
         const query = {_id: new ObjectId(id)}
@@ -67,7 +149,7 @@ run().catch(console.dir);
 
 
 
-
++
 app.get('/', (req, res) => {
   res.send('online is busy marketing')
 })
